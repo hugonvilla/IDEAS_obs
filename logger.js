bd196e5ed6387b7a49f6b68d756e2ebee3065b82
hugonvilla <35316608+hugonvilla@users.noreply.github.com>
2025-01-27 09:24:44 -0500

    http com

--- logger.js
+++ logger.js
@@ -1,9 +1,11 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const Barnowl = require('barnowl');
+const BarnowlMinew = require('barnowl-minew');
 const Barnacles = require('barnacles');
 const BarnaclesLogfile = require('barnacles-logfile');
 const Raddec = require('raddec');
 const RaddecFilter = require('raddec-filter');
+const express = require('express');
+const http = require('http');
 
 
 // -----------------------------------------------------------------------------
@@ -11,9 +13,9 @@ const RaddecFilter = require('raddec-filter');
 // acceptedTransmitterSignatures and add all devices signatures to the array
 // -----------------------------------------------------------------------------
 const FILTER_PARAMETERS = {
-    //acceptedTransmitterSignatures: [ 'e55264d41659/3', '112233445566/3' ]
+    // acceptedTransmitterSignatures: [ 'e55264d41659/0', '112233445566/0' ]
 }
-const RADDEC_PORT = process.env.RADDEC_PORT || 50001;
+
 const BARNACLES_OPTIONS = {
     packetProcessors: [
       { processor: require('advlib-ble'),
@@ -28,17 +30,19 @@ let filter = new RaddecFilter(FILTER_PARAMETERS);
 let barnacles = new Barnacles(BARNACLES_OPTIONS);
 barnacles.addInterface(BarnaclesLogfile, BARNACLES_LOGFILE_OPTIONS);
 
-server.on('message', function(msg) {
-  try {
-    let raddec = new Raddec(msg);
+let barnowl = new Barnowl({ enableMixing: true });
+let app = express();
+let server = http.createServer(app);
+server.listen(3001, function() { console.log('Listening on port 3001'); });
+let options = { app: app, express: express, route: "/minew",
+                isPreOctetStream: false }; // Set true for G1 firmware v2/3
+barnowl.addListener(BarnowlMinew, {}, BarnowlMinew.HttpListener, options);
 
+barnowl.on('raddec', (raddec) => {
+  raddec.timestamp = Date.now();
+  //console.log(raddec);
     if((raddec !== null) && filter.isPassing(raddec)) {
+      raddec.transmitterIdType = Raddec.identifiers.TYPE_RND48;
       barnacles.handleRaddec(raddec);
     }
-  }
-  catch(error) {};
 });
-
-server.bind(RADDEC_PORT);
-
-console.log('barnacles instance is listening for raddecs on port', RADDEC_PORT);
